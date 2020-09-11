339ed81d731e68bbf04bbfc64e7c17f63c40313d
Barbara Moore <barbaraloismoore@gmail.com>
2020-09-11 04:44:51 -0400

    unable to figure out BFS to find a node with ?

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -13,8 +14,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,8 +28,44 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+opposite = {
+    'n': 's',
+    'e': 'w',
+    's': 'n',
+    'w': 'e'
+}
+
+
+def lets_go(path=[], visited={}, prior_room=None, prior_direction=None, short_path=[]):
+    print('went from', prior_room, 'to', player.current_room.id)
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = {}
+        for direction in player.current_room.get_exits():
+            visited[player.current_room.id][direction] = '?'
+    if prior_room is not None:
+        visited[prior_room][prior_direction] = player.current_room.id
+        visited[player.current_room.id][opposite[prior_direction]] = prior_room
+    for direction in visited[player.current_room.id]:
+        if visited[player.current_room.id][direction] == '?':
+            print('moving', direction)
+            path.append(direction)
+            prior_room = player.current_room.id 
+            prior_direction = direction
+            player.travel(direction)
+            lets_go(path, visited, prior_room, prior_direction)
+    # print('visited', visited)    
+    path.append(opposite[prior_direction])
+    prior_room = player.current_room.id
+    prior_direction = path[-1]
+    print('moving:', path[-1])
+    player.travel(prior_direction)
+    print('went from', prior_room, 'to', player.current_room.id)
+    # print('path', path)
+
+    return path
+
+traversal_path = lets_go()
 
 
 # TRAVERSAL TEST
@@ -51,12 +88,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
